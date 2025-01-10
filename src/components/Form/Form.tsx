4e3c287786564492f976dda36b50d30cbd9173f1
Ariel Hurtado <arielhurtado388@gmail.com>
2025-01-09 23:54:07 -0500

    Inicio: Formulario de clima sin estilos

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,26 @@
+import { countries } from "../../data/countries";
+
+export default function Form() {
+  return (
+    <form>
+      <div>
+        <label htmlFor="city">Ciudad: </label>
+        <input id="city" type="text" name="city" placeholder="Ciudad" />
+      </div>
+
+      <div>
+        <label htmlFor="country">País: </label>
+        <select name="country" id="country">
+          <option value="">-- Seleccione un país</option>
+          {countries.map((country) => (
+            <option key={country.code} value={country.code}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <input type="submit" value="Consultar clima" />
+    </form>
+  );
+}
